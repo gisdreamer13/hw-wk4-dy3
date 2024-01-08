30b116833f9c6923b9907cdf78b48659268f3a1c
Your Name <gisdreamer13@gmail.com>
2024-01-08 13:44:28 -0700

    resubmission

--- .vscode/hw-wk4-dy3.sql
+++ .vscode/hw-wk4-dy3.sql
@@ -1,37 +1,55 @@
 1. List all customers who live in Texas (use
 JOINs)
 
-select * from address
-where district = 'Texas'
--- 5 customers live in Texas
+select first_name, last_name, district
+from customer
+inner join address
+on customer.address_id = address.address_id
+where district = 'Texas';
+
+--these 5 customers
 
 
 2. Get all payments above $6.99 with the Customers Full
 Name
 
-select customer_id, first_name, last_name
+select first_name, last_name, amount
 from customer
 INNER JOIN payment
-ON payment.payment_id = customer.customer_id
-having sum(amount) > 6.99
+ON customer.customer_id = payment.customer_id
+where amount > 7;
+
+--35 customers
 
 3. Show all customers names who have made payments over $175(use
 subqueries)
 
 
-SELECT * 
-FROM payment
-INNER JOIN customer
-on payment.payment_id = customer.customer_id
-having sum(amount) > 175
+SELECT first_name, last_name, amount
+FROM customer
+INNER JOIN payment
+ON customer.customer_id = payment.customer_id
+where amount > 175;
 
+--26 customers
 
 
 4. List all customers that live in Nepal (use the city
 table)
 
-select * from address
-where district = 'Nepal'
+SELECT country, first_name, last_name
+FROM customer
+INNER JOIN address
+on address.address_id = customer.address_id
+INNER JOIN city
+on address.city_id = city.city_id
+INNER JOIN country
+on city.country_id = country.country_id
+where country = 'Nepal';
+
+-- Kevin Schuler
+
+
 
 5. Which staff member had the most
 transactions?
@@ -47,11 +65,11 @@ on payment.payment_id = payment.staff_id(
 6. How many movies of each rating are
 there?
 
-select rating from film
+select rating, title
+from film
 group by rating, title
 
-select * from film
-group by film.film_id.rating
+--1001
 
 
 7.Show all customers who have made a single payment
